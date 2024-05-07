b4e38c89141d835b927797f2cffa715b6874902c
myKollege <mykollegeindia@gmail.com>
2024-05-07 14:09:38 +0600

    app namge changed

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 //============ api routes ====================
-const notification_route_1 = __importDefault(require("./app/module/notification/notification.route"));
-const user_route_1 = __importDefault(require("./app/module/user/user.route"));
-const event_route_1 = __importDefault(require("./app/module/event/event.route"));
-const feedback_route_1 = __importDefault(require("./app/module/feedback_structure/feedback.route"));
-const user_feedback_route_1 = __importDefault(require("./app/module/user_feedback/user_feedback.route"));
+const notification_route_1 = __importDefault(
+  require("./apis/module/notification/notification.route")
+);
+const user_route_1 = __importDefault(require("./apis/module/user/user.route"));
+const event_route_1 = __importDefault(
+  require("./apis/module/event/event.route")
+);
+const feedback_route_1 = __importDefault(
+  require("./apis/module/feedback_structure/feedback.route")
+);
+const user_feedback_route_1 = __importDefault(
+  require("./apis/module/user_feedback/user_feedback.route")
+);
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 //middle ware
