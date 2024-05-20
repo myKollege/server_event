cdb2fed6e8181adf14ebba0b8b01a97df6601716
myKollege <mykollegeindia@gmail.com>
2024-05-20 18:19:32 +0600

    user activity added

--- apis/module/user/user.interface.js
+++ apis/module/user/user.interface.js
@@ -1,28 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// export interface IUser {
-//   eventUserId: string;
-//   name: string;
-//   eventId: string;
-//   email?: string;
-//   phone: string;
-//   companyName: string;
-//   userGroupName: string;
-//   password: string;
-//   bio: string;
-//   profileImageUrl: string;
-//   dateOfBirth: string;
-//   shopName: string;
-//   mealPreference: string;
-//   city: string;
-//   state: string;
-//   zone: string;
-//   totalMembers: number;
-//   internationalHub: string;
-//   panCardImageUrl: string;
-//   passportImageUrl: string;
-//   passportSizeImageUrl: string;
-//   bookmarks: string[];
-//   gender: "male" | "female" | "other";
-//   type: "consumer" | "admin" | "employee";
-// }
+var ActivityType;
+(function (ActivityType) {
+    ActivityType[ActivityType["LOGIN"] = 0] = "LOGIN";
+    ActivityType[ActivityType["LOGOUT"] = 1] = "LOGOUT";
+    ActivityType[ActivityType["SIGNUP"] = 2] = "SIGNUP";
+    ActivityType[ActivityType["PROFILE_UPDATE"] = 3] = "PROFILE_UPDATE";
+    ActivityType[ActivityType["PASSWORD_CHANGE"] = 4] = "PASSWORD_CHANGE";
+    ActivityType[ActivityType["TERMS_CHECKED"] = 5] = "TERMS_CHECKED";
+})(ActivityType || (ActivityType = {}));
